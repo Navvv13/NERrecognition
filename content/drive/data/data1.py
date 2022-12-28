0ad378474e4637a6e308cb349469288e9ab7d305
Navvv13 <navneetassi01331@gmail.com>
2022-12-28 15:34:23 -0400

    initial commit

---
+++ content/drive/data/data1.py
@@ -0,0 +1,21 @@
+
+train = [
+          ("I took 30 ml of A Class syrup as prescribed by my doctor to relieve my cough and congestion.",{"entities":[(7,12,"quantity"),(16,29,"medicine")]}),
+          ("I mixed 30 ml of A Class syrup with a glass of water and drank it before bed to help me sleep.",{"entities":[(8,13,"quantity"),(17,30,"medicine")]}),
+          ("The pharmacist recommended that I take 30 ml of A Clas syrup three times a day to treat my cold symptoms.",{"entities":[(39,44,"quantity"),(47,60,"medicine")]}),
+          ("I added 30 ml of A Clas syrup to a bottle of water and shook it up to create a refreshing and medicinal drink.",{"entities":[(8,13,"quantity"),(17,29,"medicine")]}),
+          ("I measured out 30 ml of A Clas syrup and mixed it with some juice to make it more palatable for my child's medicine.",{"entities":[(15,20,"quantity"),(24,37,"medicine")]}),
+          ("The doctor prescribed an A Artivid 150mg injection to help control my chronic pain.",{"entities":[(35,40,"quantity"),(25,34,"medicine")]}),
+          ("I had to go to the hospital to get an A Artivid 150mg injection because it is not available in oral form.",{"entities":[(48,53,"quantity"),(38,47,"medicine")]}),
+          ("The A Artivid 150mg injection worked quickly to provide relief from my acute pain.",{"entities":[(14,19,"quantity"),(4,13,"medicine")]}),
+          ("I received an A Artivid 150mg injection to help reduce inflammation and swelling in my joints.",{"entities":[(24,30,"quantity"),(13,23,"medicine")]}),
+          ("The doctor prescribed 0.5 mg of A Cort tablets to help reduce inflammation and swelling in my sinuses.",{"entities":[(22,28,"quantity"),(32,38,"medicine")]}),
+          ("I have to be careful not to exceed the recommended dosage of 0.5 mg for the A Cort tablets.",{"entities":[(61,67,"quantity"),(76,82,"medicine")]}),
+          ("I take 0.5 mg of A Cort tablets before bed to help me breathe easier at night.",{"entities":[(7,14,"quantity"),(17,24,"medicine")]}),
+          ("I find that 0.5 mg of A Cort tablets is the perfect amount to control my allergy symptoms without causing drowsiness.",{"entities":[(12,18,"quantity"),(22,28,"medicine")]}),
+          ("The 200 mg of A C Ford Kid tablets have helped to significantly improve my child's respiratory function.",{"entities":[(4,10,"quantity"),(14,26,"medicine")]}),
+          ("I make sure to give my child their 200 mg of A C Ford Kid tablets at the same time every day to help maintain a consistent level of the medication in their system.",{"entities":[(35,41,"quantity"),(45,57,"medicine")]}),
+          ("My child takes 200 mg of A C Ford Kid tablets to help with their asthma symptoms.",{"entities":[(15,21,"quantity"),(25,38,"medicine")]}),
+          ("I appreciate that the A C Ford Kid tablets come in a convenient 200 mg dosage that is easy for my child to swallow.",{"entities":[(64,70,"quantity"),(22,34,"medicine")]}),
+        ]
+
